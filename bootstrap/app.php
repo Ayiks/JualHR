1dd7316865d532674a9ec9a32682322d0d92669e
Ayiks <cliffysarp@gmail.com>
2025-12-20 08:26:57 +0000

    added role checks for login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+            'manager' => \App\Http\Middleware\EnsureUserIsManager::class,
+            'active_employee' => \App\Http\Middleware\CheckEmployeeStatus::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
