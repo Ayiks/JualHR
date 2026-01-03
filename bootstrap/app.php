aeac25441a7319e710d5e0dc8ea69b9b1116459d
Ayiks <cliffysarp@gmail.com>
2026-01-03 22:40:41 +0000

    tbc

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\ForcePasswordReset;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
             'active_employee' => \App\Http\Middleware\CheckEmployeeStatus::class,
         ]);
     })
+->withMiddleware(function (Middleware $middleware) {
+    $middleware->append(ForcePasswordReset::class);
+})
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
